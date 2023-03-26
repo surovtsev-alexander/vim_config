a3511ab80495164e7882bef10d0e29ebb172f4a8
Alexander Surovtsev <alexander.surovtsev.vrn@gmail.com>
2023-03-26 11:53:29 +0100

    initial commit

---
+++ .vim_fix_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#tr -d '\015' <./pathogen.vim > ./pathogen.vim1
+
+#install vundle
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ets-labs/python-vimrc/master/setup.sh)"
+function main
+{
+
+}
+
+main
